238712894be9cc7b877be0344e33adf6c4fc7920
Callum <callum@madebymany.co.uk>
2016-02-19 10:36:27 +0000

    fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+VERSION=v$(bin/version.js)
+echo Building version $VERSION
 electron-packager . Version --platform=darwin --arch=x64 --version=0.36.7 --out=build --overwrite
-zip -rX build/Version-v$(bin/version.js)-darwin-x64.zip build/Version-darwin-x64/Version.app/
+cd build/Version-darwin-x64/
+zip -rX ../Version-$VERSION-darwin-x64.zip Version.app/
